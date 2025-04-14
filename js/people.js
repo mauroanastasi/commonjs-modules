47381039d84e43c85dd73a4d9c82fa6a9713cec5
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-14 18:24:07 +0200

    [UPD] modulo 3 importo funzioni

---
+++ js/people.js
@@ -0,0 +1,7 @@
+// importo le funzioni create nei due file js precedenti 
+const firstFunction = require(`./names`);
+const secondFunction = require(`./hobbies`);
+
+// verifico in terminale inserendo node js/people che abbia importato le due funzioni 
+
+
