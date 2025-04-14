6ebee7d4485f3e278176af2d035c8ad346499a13
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-14 18:11:19 +0200

    [UPD] modulo 2

---
+++ js/hobbies.js
@@ -0,0 +1,7 @@
+function hobbies (hobbyOne, hobbyTwo, hobbyThree){
+    const whatsHobbies = [hobbyOne, hobbyTwo, hobbyThree]
+    return whatsHobbies
+}
+
+const myhobbies = hobbies("videogames", "cinema", "food")
+console.log(myhobbies)
